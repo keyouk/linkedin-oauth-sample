801115ba71857f31a986601d3717c15e843b0090
Kelvin Youk <kelvin@kelvin-youk.local>
2018-09-12 23:41:59 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,17 @@
+import os
+import requests
+from flask import Flask, request, jsonify, abort, render_template, url_for
+
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route('/', methods=['GET'])
+def home():
+	return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+	app.debug = True
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
